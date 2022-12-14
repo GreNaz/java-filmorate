37b6e7e38a3e04bf728350212cb63ace7139ff37
GreNaz <nazargrehov@gmail.com>
2022-12-14 22:28:36 +0300

    testGetUsers

--- src/test/java/ru/yandex/practicum/filmorate/storage/film/impl/FilmDbStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/impl/FilmDbStorageTest.java
@@ -9,9 +9,11 @@ import ru.yandex.practicum.filmorate.model.User;
 import ru.yandex.practicum.filmorate.storage.user.impl.UserDbStorage;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -20,7 +22,13 @@ class FilmDbStorageTest {
 
     @Test
     void testFindUserById() {
-        User user = new User(1,"mail@mail.ru","userLogin","userName", LocalDate.now().minusYears(25));
+
+        User user = new User(1L,
+                "mail@mail.ru",
+                "userLogin",
+                "userName",
+                LocalDate.now().minusYears(25));
+
         userStorage.create(user);
         Optional<User> userOptional = userStorage.get(1L);
 
@@ -30,4 +38,32 @@ class FilmDbStorageTest {
                         assertThat(testUser).hasFieldOrPropertyWithValue("id", 1L)
                 );
     }
+
+    @Test
+    void testGetUsers() {
+        User user1 = new User(1L,
+                "1mail@mail.ru",
+                "userLogin1",
+                "userName1",
+                LocalDate.now().minusYears(25));
+        User user2 = new User(2L,
+                "2mail@mail.ru",
+                "userLogin2",
+                "userName2",
+                LocalDate.now().minusYears(30));
+        User user3 = new User(3L,
+                "3mail@mail.ru",
+                "userLogin3",
+                "userName3",
+                LocalDate.now().minusYears(40));
+        
+        userStorage.create(user1);
+        userStorage.create(user2);
+        userStorage.create(user3);
+
+        List<User> users = userStorage.getUsers();
+
+        assertThat(users.containsAll(List.of(user1, user2, user3))).isTrue();
+    }
+
 }
